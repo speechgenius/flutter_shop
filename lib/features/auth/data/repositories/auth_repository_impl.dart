f14824310a09cbace88e6807dc1b0b5112ca4cbf
speechgenius <shaabandaudi@gmail.com>
2026-04-01 00:55:24 +0300

    added AuthRepository contract

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,12 +1,14 @@
 import '../datasources/auth_remote_datasource.dart';
 import '../../../core/network/token_storage.dart';
+import '../domain/repositories/auth_repository.dart';
 
-class AuthRepositoryImpl {
+class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource remote;
   final TokenStorage storage;
 
   AuthRepositoryImpl(this.remote, this.storage);
 
+  @override
   Future<void> login(String email, String password) async {
     final data = await remote.login(email, password);
 
@@ -19,11 +21,13 @@ class AuthRepositoryImpl {
     await storage.save(token);
   }
 
+  @override
   Future<void> logout() async {
     await remote.logout();
     await storage.delete();
   }
 
+  @override
   Future<bool> isLoggedIn() async {
     return await storage.hasToken();
   }
